5b864d60cc4e458666f87215b210103d58b69bdb
maulik-dodia <mmdoida@gmail.com>
2020-12-22 22:49:56 +0530

    1. GlideModule issue solved 2. Removed unwanted dependencies 3. Other small changes

--- app/src/main/java/com/jutaav/activity/WSLoadingActivity.kt
+++ app/src/main/java/com/jutaav/activity/WSLoadingActivity.kt
@@ -1,12 +1,12 @@
 package com.jutaav.activity
 
 import android.content.Intent
-import com.bumptech.glide.Glide
 import com.jutaav.R
 import com.jutaav.base.extensions.tag
 import com.jutaav.baseandroid.BaseActivity
 import com.jutaav.databinding.ActivityWSLoadingBinding
 import com.jutaav.task.TaskCompleteActivity
+import com.jutaav.utils.GlideApp
 import com.wada811.viewbinding.viewBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
@@ -21,7 +21,7 @@ class WSLoadingActivity : BaseActivity<ActivityWSLoadingBinding>(R.layout.activi
         Timber.tag(tag()).i("${this::class.java.simpleName} Opened")
         try {
 
-            Glide.with(this)
+            GlideApp.with(this)
                 .load(R.raw.giphy6)
                 .into(binding.ivLoading)
 
